253947bf072a0cec598579c6663c4338cabfa1b8
JackVoron <99660099+JackVoron@users.noreply.github.com>
2023-03-01 21:26:34 +0300

    Add HP

--- Assets/Sources/Presenter/ShipPresenter.cs
+++ Assets/Sources/Presenter/ShipPresenter.cs
@@ -3,17 +3,27 @@
 public class ShipPresenter : Presenter
 {
     private Root _init;
+    private int _healthPoints;
 
     public void Init(Root init)
     {
         _init = init;
+        _healthPoints = 3;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            _init.DisableShip();
+            _healthPoints--;
+            if (_healthPoints-- > 0)
+            {
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                _init.DisableShip();
+            }
         }
     }
 }
